7d1b755e287460dc1ce07daee80c41c412c099a5
Vitória <vitoria1.silva.costa@gmail.com>
2022-10-05 12:04:07 -0300

    alteracoes no teste automatizado

--- cypress/cypress/e2e/integration/pre-cadastro.cy.js
+++ cypress/cypress/e2e/integration/pre-cadastro.cy.js
@@ -9,13 +9,17 @@ describe('Funcionalidade Pré Cadastro', () => {
     });
 
     it.only('Deve completar o Pré Cadastro com sucesso', () => {
-        cy.get('#reg_email').type(faker.internet.email())
+        let nomeFaker = faker.name.firstName()
+        let sobrenomeFaker = faker.name.lastName()
+        let emailFaker = faker.internet.email(nomeFaker)
+
+        cy.get('#reg_email').type(emailFaker)
         cy.get('#reg_password').type('teste@teste.com')
         cy.get(':nth-child(4) > .button').click()
 
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-        cy.get('#account_first_name').type(faker.name.firstName())
-        cy.get('#account_last_name').type(faker.name.lastName())
+        cy.get('#account_first_name').type(nomeFaker)
+        cy.get('#account_last_name').type(sobrenomeFaker)
         cy.get('.woocommerce-Button').click()
         cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso')
     });
