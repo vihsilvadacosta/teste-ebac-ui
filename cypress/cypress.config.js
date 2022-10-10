7027a7f7fca3881ccb4e955545c81f5f552f51d6
Vitória <vitoria1.silva.costa@gmail.com>
2022-10-10 11:25:04 -0300

    usando massa de dados em lista

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  chromeWebSecurity: false
   projectId: 'zwoj81',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+  
     },
   },
 });
--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+import enderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
 
@@ -12,6 +14,22 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
         
 
     it('Deve fazer cadastro de faturamento com sucesso', () => {
-        
+        enderecoPage.editarEnderecoFaturamento('Laura', 'Almeida', 'Twitter', 'Brasil', 'R Luiz Fernandes', '67', 'Ferraz', 'São Paulo', '08546971', '11950824578', 'vivivane@dominio.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
+    });
+    it('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        enderecoPage.editarEnderecoFaturamento(dadosEndereco[1].nome,
+        dadosEndereco[1].sobrenome,
+        dadosEndereco[1].empresa,
+        dadosEndereco[1].pais,
+        dadosEndereco[1].endereco,
+        dadosEndereco[1].numero,
+        dadosEndereco[1].cidade,
+        dadosEndereco[1].estado,
+        dadosEndereco[1].cep,
+        dadosEndereco[1].telefone,
+        dadosEndereco[1].email
+        )
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
     });
 });
